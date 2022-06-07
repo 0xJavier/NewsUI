036cafc10103304b520eeb70fd38e65c592eaa7b
Javier <0xJMunoz@gmail.com>
2022-06-07 11:46:08 -0400

    add: skeleton project

---
+++ Shared/NewsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsUIApp.swift
+//  Shared
+//
+//  Created by Javier Munoz on 6/6/22.
+//
+
+import SwiftUI
+
+@main
+struct NewsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
